6b3961f98a4c7afc41bc9d1a33f0b0a9f0aa0e08
Alexander Dines <Dynasty9@users.noreply.github.com>
2024-02-02 03:57:48 +0100

    updated demo

--- sunshine_direct.py
+++ sunshine_direct.py
@@ -1,18 +1,49 @@
+import json
+import logging
+
+import openai
 import runloop
-from openai import OpenAI
+
+"""
+The openai app implementation.
+"""
+
 _SYSTEM_MSG = {"role": "system",
-               "content": "You are a kind, helpful travel who makes travel suggestions based on a users' prefered weather. " +
-               "If you don't know the users' prefered weather, you should probably ask for it!" }
+               "content": "You are a laughably unhelpful travel agent assisting users to find the best value vacation "
+                          "possible."
+                          "Using the tools available to you, you work with the user to identify the best round trip "
+                          "flight option that aligns to the user's preferences. If you don't know their preferences "
+                          "or need further information to give a good recommendation, you should ask the user "
+                          "a question."}
 
 
-openAIClient = OpenAI()
+_HISTORY_KEY = "history"
+
+logger = logging.getLogger(__name__)
+_model = "gpt-3.5-turbo-16k"
+_client = openai.OpenAI()
+
 
 @runloop.loop
-def travel_agent(metadata: dict[str, str], greeting: list[str]) -> tuple[list[str], dict[str, str]]:
-    completion = openAIClient.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[_SYSTEM_MSG] + session.history + { "role": "user", "content": message },
+def chat(
+    metadata: dict[str, str],
+    inputs: list[str]
+) -> tuple[list[str], dict[str, str]]:
+    logger.debug(f"handle_input metadata={metadata} input={inputs}")
+    next_user_line = {"role": "user", "content": inputs[0]}
+    existing_non_system_messages = json.loads(metadata.get(_HISTORY_KEY, "[]"))
+
+    messages_to_process = existing_non_system_messages + [next_user_line]
+    response = _client.chat.completions.create(
+        model=_model,
+        messages=[_SYSTEM_MSG] + messages_to_process,
     )
-    
-    # Update our session history & respond to our client with new messages
-    return [completion.choices[0].message.content], metadata
+
+    logger.debug(f"got response={response}")
+    # TODO: determine how to propagate errors
+    ai_response_message = response.choices[0].message
+
+    metadata[_HISTORY_KEY] = json.dumps(
+        messages_to_process + [ai_response_message.model_dump_json()])
+
+    return [ai_response_message.content], metadata
