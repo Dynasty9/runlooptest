56c543a73c815b91c3259ae1d6d25676ecc5f455
Alexander Dines <Dynasty9@users.noreply.github.com>
2024-02-02 04:22:21 +0100

    new code

--- sunshine_direct.py
+++ sunshine_direct.py
@@ -4,47 +4,35 @@ import logging
 import openai
 import runloop
 
+from openai.types.chat import ChatCompletionUserMessageParam, ChatCompletionSystemMessageParam, ChatCompletionAssistantMessageParam, ChatCompletionMessageParam
+
+
 """
 The openai app implementation.
 """
 
-_SYSTEM_MSG = {"role": "system",
-               "content": "You are a laughably unhelpful travel agent assisting users to find the best value vacation "
-                          "possible."
-                          "Using the tools available to you, you work with the user to identify the best round trip "
-                          "flight option that aligns to the user's preferences. If you don't know their preferences "
-                          "or need further information to give a good recommendation, you should ask the user "
-                          "a question."}
-
-
-_HISTORY_KEY = "history"
+_SYSTEM_MSG = ChatCompletionSystemMessageParam(content="You are a degenerate bookie with acute math skills", role="system")
 
 logger = logging.getLogger(__name__)
-_model = "gpt-3.5-turbo-16k"
+_model = "gpt-4-1106-preview"
 _client = openai.OpenAI()
 
 
 @runloop.loop
-def chat(
-    metadata: dict[str, str],
-    inputs: list[str]
-) -> tuple[list[str], dict[str, str]]:
-    logger.debug(f"handle_input metadata={metadata} input={inputs}")
-    next_user_line = {"role": "user", "content": inputs[0]}
-    existing_non_system_messages = json.loads(metadata.get(_HISTORY_KEY, "[]"))
+def open_ai_data(metadata: dict[str, str], input: list[str]) -> tuple[list[str], dict[str, str]]:
+    print(metadata)
+    user_message = ChatCompletionUserMessageParam(content=input[0], role="user")
 
-    messages_to_process = existing_non_system_messages + [next_user_line]
-    logger.debug(f"Messages:{messages_to_process}")
+    history = json.loads(metadata.get("history", "[]"))
+
     response = _client.chat.completions.create(
         model=_model,
-        messages=[_SYSTEM_MSG] + messages_to_process,
+        messages=[_SYSTEM_MSG] + history + [user_message],
     )
+    response.choices[0].message.model_dump_json()
 
-    logger.debug(f"got response={response}")
-    # TODO: determine how to propagate errors
-    ai_response_message = response.choices[0].message
+    metadata["history"] = json.dumps(history + [user_message] +
+                                     [ChatCompletionAssistantMessageParam(content=response.choices[0].message.content,
+                                                                          role="assistant")])
 
-    metadata[_HISTORY_KEY] = json.dumps(
-        messages_to_process + [ai_response_message.model_dump_json()])
-
-    return [ai_response_message.content], metadata
+    return [response.choices[0].message.content], metadata
